2b998a46a606f66caef3fffc9f8c5110085c1942
krish343407 <krish.076@hotmail.com>
2026-01-04 08:46:33 +0530

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVesrsion = 100.2F;
+		String browserName = "firefox";
+		int releaseYear = 1998;
+		char browserLogo = 'F';
+		boolean isVisible = true;
+		
+			System.out.println(browserName+'\n'+browserVesrsion+'\n'+releaseYear+'\n'+browserLogo+'\n'+isVisible);	
+				
+
+	}
+
+}
